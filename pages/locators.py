770feef455708dc509fe9e47d67c8afc1da52255
Denis Antonov <denant-inside@yandex.ru>
2025-06-18 11:49:58 +0300

    Реализованы тесты для проверки url-адреса и наличия форм на странице авторизации

---
+++ pages/locators.py
@@ -0,0 +1,21 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    LOGIN_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
+    LOGIN_BTN = (By.CSS_SELECTOR, "button[value='Log In']")
+
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTER_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_PASSWORD_1 = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTER_PASSWORD_2 = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTER_BTN = (By.CSS_SELECTOR, "button[value='Register']")
+
+
+

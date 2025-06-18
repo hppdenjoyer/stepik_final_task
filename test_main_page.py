4088bbb60e717b9753224ac6d36daccab065f489
Denis Antonov <denant-inside@yandex.ru>
2025-06-18 15:06:41 +0300

    Реализован переход между главное страницей и страницей авторизации при выполнении тестов

--- test_main_page.py
+++ test_main_page.py
@@ -4,12 +4,11 @@ from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    # инициализируем Page Object
     page = MainPage(browser, link)
-    # открываем страницу
     page.open()
-    # выполняем метод страницы — переходим на страницу логина
     page.go_to_login_page()
+    login_page = LoginPage(browser, self.browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
@@ -19,25 +18,7 @@ def test_guest_should_see_login_link(browser):
     page.should_be_login_link()
 
 
-def test_current_page_should_be_login_url(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_url()
 
-
-def test_should_be_login_form_on_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_form()
-
-
-def test_should_be_register_form_on_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_register_form()
 
 
 

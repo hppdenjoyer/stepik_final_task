13d4047ba7372553bd3f2a053c096f6e480cb4a6
Denis Antonov <denant-inside@yandex.ru>
2025-06-20 22:09:04 +0300

    Реализован тест проверки, что у неавторизованного пользователя, перешедшего со страницы товара в корзину, корзина будет пуста

--- test_product_page.py
+++ test_product_page.py
@@ -1,22 +1,8 @@
 import pytest
 from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
 
 
-# @pytest.mark.parametrize('link', [
-#     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-#     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-#     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-#     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-#     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-#     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-#     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-#     pytest.param(
-#         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", 
-#         marks=pytest.mark.xfail
-#     ),
-#     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-#     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
-# ])
 def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
@@ -29,6 +15,17 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_not_be_products_in_basket()
+    basket_page.should_be_basket_empty_message()
+    
 
 
 @pytest.mark.xfail
